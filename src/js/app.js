e13cc3e1f125e5800bc7a176f052f6416bf4c3a8
JNACHO2001 <joseesapel@gmail.com>
2025-06-20 13:58:20 -0500

    faat:metas agregados en el index

--- src/js/app.js
+++ src/js/app.js
@@ -9,23 +9,23 @@ entrar.addEventListener('click', function () {
 
     let mensaje;
 
-    if (nombreIngresado.length==0 || edadIngresada.length==0) {
+    if (nombreIngresado.length == 0 || edadIngresada.length == 0) {
 
-        mensaje="!Por favor ingreesar un campo valido!"
+        mensaje = "!Por favor ingreesar un campo valido!"
 
 
-        
-    }else if(edadIngresada<18) {
-        mensaje="Hola " + nombreIngresado+ " eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!"
-        
+
+    } else if (edadIngresada < 18) {
+        mensaje = "Hola " + nombreIngresado + " eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!"
+
     } else {
-        mensaje="Hola "  + nombreIngresado + " eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!"
-        
+        mensaje = "Hola " + nombreIngresado + " eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!"
+
     }
 
-    
-    
-    
+
+
+
 
     resultado.textContent = mensaje;
 });
